bb9dcbfc46a793f6c0f717e09a19c0b04b7cd5dd
fisidinho25 <flywaterz09@gmail.com>
2022-09-14 08:03:25 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,12 +5,11 @@
  * Return: Always 0 (success)
  */
 void print_alphabet(void)
-
 {
-	int n;
-	for (n = 'a'; n <= 'z'; n++)
-	{	
-	_putchar('n');
-		}
-	_putchar('\n');
+int n;
+for (n = 'a'; n <= 'z'; n++)
+{	
+_putchar('n');
+}
+_putchar('\n');
 }
