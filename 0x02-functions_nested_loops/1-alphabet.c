fac266b5e70c4ede9d481e2a2d00a49056c6b3bd
fisidinho25 <flywaterz09@gmail.com>
2022-09-14 07:53:59 -0700

    print_alphabet - a function that prints the alphabet, in lowercase.

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,16 @@
 #include "main.h"
 /**
- * main - check the code for Holberton School students.
+ * print_alphabet - a function that prints the alphabet, in lowercase.
  *
- * Return: Always 0.
+ * Return: Always 0 (success)
  */
+void print_alphabet(void)
 
-int main(void)
 {
-print_alphabet();
-return (0);
+	int n;
+	for (n = 'a'; n <= 'z'; n++)
+	{	
+	_putchar('n');
+		}
+	_putchar('\n')
 }
