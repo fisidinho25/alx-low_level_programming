db66fd5474eb6347b44bf97dd0aba4044cc6e17e
fisidinho25 <flywaterz09@gmail.com>
2022-09-14 09:40:31 -0700

    task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -15,9 +15,14 @@ int _isalpha(int c)
 char lower, upper;
 int isletter = 0;
 for (lower = 'a'; lower <= 'z'; lower++)
+
 {
 for (upper = 'A'; upper <= 'Z'; upper++)
-{										if (c == lower || c == upper)							isletter = 1;
-}										}
+{
+if (c == lower || c == upper)
+isletter = 1;
+}
+
+}
 return (isletter);
 }
